5ef48b04a7d148fc9722d7e3ab4801e6eb719bb8
OlegBliznyuk <oleg.v.bliznyuk@gmail.com>
2020-07-06 22:03:09 +0300

    Update telegramsend.py
    add telegramm acho send with "NOT"

--- telegramsend.py
+++ telegramsend.py
@@ -1,7 +1,15 @@
-import telepot
+import telebot
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+@bot.message_handler(commands=["start"])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Здравчтвуй новый пользователь это телеграм бот основаный на игре Поле чудес")
 
 
-token = ''
-TelegramBot = telepot.Bot(token)
-#print(TelegramBot.getMe())
-print(TelegramBot.getUpdates())
+
+@bot.message_handler(content_types=["text"])
+def handle_start(message):
+    bot.send_message(message.chat.id, "не " + message.text)
+
+bot.polling(none_stop=True)
