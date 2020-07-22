42189052eae9862beb103a7f9abf3ad425003a65
OlegBliznyuk <oleg.v.bliznyuk@gmail.com>
2020-07-22 23:13:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-i = ""
-answer_out = ""
-word = "автомобиль"
+import random
+
+
+
 def open_letter(word, answer_out, polz):
     wordlist = list(word)
     new_answer_out = ""
@@ -10,25 +11,42 @@ def open_letter(word, answer_out, polz):
             new_answer_out = new_answer_out + wordlist[i]
         else:
             new_answer_out = new_answer_out + answer_out[i]
-#            wordlist = list(answer_out)
+ #              wordlist = list(answer_out)
     return new_answer_out
+
+
 
 def make_visable_word(word):
     return  "*" * len(word)
 
 
+
+
+filename = 'zdf-win.txt'
+lines = open(filename).read().split('\n')
+i = ""
+answer_out = ""
+f_counter = len(lines)
+rand = random.randint(0,f_counter)
+word = lines[rand]
+visable = make_visable_word(word)
+
+
+
+print(f'Случайное слово номер {rand} - {visable}.')
+
+
+
+
+
+
+
+wordlist = list(word)
+answer_out = make_visable_word(word)
+#print(answer_out)
 while answer_out != word:
     polz = input("Назовите букву :")
-    for i in range(len(wordlist)):
-        fakeword = str(wordlist[i])
-        if wordlist[i] == polz:
-            answer_out = answer_out + wordlist[i]
-        elif fakeword != polz:
-            answer_out = answer_out + "*"
-        else:
-            wordlist = answer_out
-            wordlist = list(wordlist)
+    answer_out = open_letter(word,answer_out,polz)
 
-        print(answer_out)
-    print("qwe")
-print(answer_out)
+    print(answer_out)
+print(f'Ты победил! Это слово {answer_out}!')
